bc3a04ebb62a6856ab32e30d8c4e6977cabc1906
s3abiscuit <syjefb2@outlook.com>
2020-01-11 15:29:34 +0800

    Update Sort.java

--- sort/Sort.java
+++ sort/Sort.java
@@ -131,4 +131,26 @@ class Sort {
         return j;
     }
     /********************************quick sort**********************************/
+    
+    /*********************************heap sort**********************************/
+    public static void sort(Comparable[] pq) {
+        int n = pq.length;
+        for (int k = n/2; k >= 1; k--)
+            sink(pq, k, n);
+        while (n > 1) {
+            exch(pq, 1, n--);
+            sink(pq, 1, n);
+        }
+    }
+
+    private static void sink(Comparable[] pq, int k, int n) {
+        while (2*k <= n) {
+            int j = 2*k;
+            if (j < n && less(pq, j, j+1)) j++;
+            if (!less(pq, k, j)) break;
+            exch(pq, k, j);
+            k = j;
+        }
+    }
+    /*********************************heap sort**********************************/
 }
