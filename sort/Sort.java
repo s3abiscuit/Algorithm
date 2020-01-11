131d719e610a05d02f14a24b35df62ab2557b033
s3abiscuit <syjefb2@outlook.com>
2020-01-11 10:55:51 +0800

    Update Sort.java

--- sort/Sort.java
+++ sort/Sort.java
@@ -44,4 +44,43 @@ class Sort {
         }
         assert isSorted(a);
     }
+    
+    /********************************merge sort**********************************/
+    public static void merge (Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length-1);
+        assert isSorted(a);
+    }
+    
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+    
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        // precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
+        assert isSorted(a, lo, mid);
+        assert isSorted(a, mid+1, hi);
+
+        // copy to aux[]
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k]; 
+        }
+
+        // merge back to a[]
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)              a[k] = aux[j++];
+            else if (j > hi)               a[k] = aux[i++];
+            else if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else                           a[k] = aux[i++];
+        }
+
+        // postcondition: a[lo .. hi] is sorted
+        assert isSorted(a, lo, hi);
+    }
+    /********************************merge sort**********************************/
 }
