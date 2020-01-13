475da4f4a854046eb0e5ff0b8bff666814623b4e
s3abiscuit <syjefb2@outlook.com>
2020-01-13 18:20:34 +0800

    Create RedBlackBST.java

---
+++ sort/RedBlackBST.java
@@ -0,0 +1,32 @@
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+    // ?1 右链接有Red节点需要左旋转
+    // ?2 连续两个左Red节点需要右旋转
+    // ?3 flip
+    // ?4 插入的节点是Red
+    private static final boolean RED   = true;
+    private static final boolean BLACK = false;
+
+    private Node root;     // root of the BST
+
+    // BST helper node data type
+    private class Node {
+        private Key key;           // key
+        private Value val;         // associated data
+        private Node left, right;  // links to left and right subtrees
+        private boolean color;     // color of parent link
+        private int size;          // subtree count
+
+        public Node(Key key, Value val, boolean color, int size) {
+            this.key = key;
+            this.val = val;
+            this.color = color;
+            this.size = size;
+        }
+    }
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public RedBlackBST() {
+    }
+}
